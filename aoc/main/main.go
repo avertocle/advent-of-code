e72c8676bfa21db534e1a47cdc2e8cb062bc262a
avertocle <avertocle@gmail.com>
2022-12-13 20:56:57 +0530

    aoc main

--- aoc/main/main.go
+++ aoc/main/main.go
@@ -11,12 +11,13 @@ import (
 	"github.com/avertocle/contests/io/outils"
 	"log"
 	"path"
+	"strings"
 )
 
 const dirPath = "../2021/day17"
 
 func main() {
-	fmt.Printf("\nstart\n")
+	displayPrettyHeader()
 	inputFileNames, err := iutils.GetInputFileList(dirPath)
 	if err != nil {
 		log.Fatalf("error fetching input file : dir(%v) | %v", dirPath, err)
@@ -29,9 +30,11 @@ func main() {
 	for _, ifName := range inputFileNames {
 		ifPath = path.Join(dirPath, ifName)
 		ansP1, ansP2 = runForOneInputFile(ifPath)
-		displayPretty(ifPath, ansP1, ansP2)
+		displayPrettyResult(ifName, ansP1, ansP2)
 	}
 }
+
+/***** Display Functions *****/
 
 func runForOneInputFile(inputFilePath string) (string, string) {
 	prob.ParseInput(inputFilePath)
@@ -40,10 +43,29 @@ func runForOneInputFile(inputFilePath string) (string, string) {
 	return ansP1, ansP2
 }
 
-func displayPretty(ifPath, ansP1, ansP2 string) {
-	fmt.Printf("=> %v : ansP1 = %v : ans-P2 = %v\n",
-		outils.Clr(ifPath, outils.Gray),
+func displayPrettyHeader() {
+	fmt.Println()
+	fmt.Println()
+	displayPrettyHorLine()
+	fmt.Println(outils.Clr(fmt.Sprintf("Solving %v", dirPath), outils.Green))
+	fmt.Println()
+	fmt.Println(outils.Clr("<<< prog logs start >>>", outils.Yellow))
+	fmt.Println()
+}
+
+func displayPrettyResult(ifName, ansP1, ansP2 string) {
+	fmt.Println()
+	fmt.Println(outils.Clr("<<< prog logs end >>>", outils.Yellow))
+	fmt.Println()
+	displayPrettyHorLine()
+	fmt.Printf("%v : ansP1 = %v : ans-P2 = %v\n",
+		outils.Clr(ifName, outils.Yellow),
 		outils.Clr(ansP1, outils.Green),
 		outils.Clr(ansP2, outils.Green))
+	displayPrettyHorLine()
+	fmt.Println()
+}
 
+func displayPrettyHorLine() {
+	fmt.Println(outils.Clr(strings.Repeat("~-", 30)+"~", outils.Yellow))
 }
