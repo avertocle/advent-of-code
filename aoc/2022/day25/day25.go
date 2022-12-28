667603dd7ef3e13a09c7ab7ebb66a7fedd26af20
avertocle <avertocle@gmail.com>
2022-12-28 15:26:00 +0530

    aoc 2022 day25

--- aoc/2022/day25/day25.go
+++ aoc/2022/day25/day25.go
@@ -3,13 +3,25 @@ package day25
 import (
 	"fmt"
 	"github.com/avertocle/contests/io/errz"
+	"github.com/avertocle/contests/io/intz"
 	"github.com/avertocle/contests/io/iutils"
+	"math"
+	"strconv"
+	"strings"
 )
 
-var input []string
+// u5 => unbalanced 5
+// u10 => unbalanced 10
+// snafu = snafu
+
+var gInput []string
 
 func SolveP1() string {
-	ans := "0"
+	sumDec := int64(0)
+	for _, fu := range gInput {
+		sumDec += b5ToU10(snafuToB5(fu))
+	}
+	ans := b5ToSnafu(u10ToB5(sumDec))
 	return fmt.Sprintf("%v", ans)
 }
 
@@ -25,5 +37,63 @@ func SolveP2() string {
 func ParseInput(inputFilePath string) {
 	lines, err := iutils.FromFile(inputFilePath, false)
 	errz.HardAssert(err == nil, "iutils error | %v", err)
-	input = lines
+	gInput = lines
+}
+
+func b5ToU10(u5 []int) int64 {
+	dec := int64(0)
+	for i, fu := range u5 {
+		dec += int64(fu) * intz.Pow(5, len(u5)-i-1)
+	}
+	return dec
+}
+
+func u10ToB5(u10 int64) []int {
+	u5 := strings.Repeat("0", 10) + strconv.FormatInt(u10, 5)
+	b5 := iutils.ExtractInt1DFromString0D(u5, "", math.MaxInt)
+	for i := len(b5) - 1; i > 0; i-- {
+		if b5[i] >= 3 {
+			b5[i] -= 5
+			b5[i-1] += 1
+		}
+	}
+	return b5
+}
+
+func snafuToB5(snafu string) []int {
+	b5 := make([]int, len(snafu))
+	for i, fu := range snafu {
+		if fu == '-' {
+			b5[i] = -1
+		} else if fu == '=' {
+			b5[i] = -2
+		} else if fu == '0' || fu == '1' || fu == '2' {
+			b5[i] = int(fu - '0')
+		} else {
+			errz.HardAssert(false, "invalid snafu string (%v) idx(%v) byte(%v)", snafu, i, string(fu))
+		}
+	}
+	return b5
+}
+
+func b5ToSnafu(b5 []int) string {
+	snafu := make([]byte, len(b5))
+	isPadding := true
+	for i, x := range b5 {
+		if x == 0 && isPadding == true {
+			continue
+		} else {
+			isPadding = false
+		}
+		if x == -1 {
+			snafu[i] = '-'
+		} else if x == -2 {
+			snafu[i] = '='
+		} else if x == 0 || x == 1 || x == 2 {
+			snafu[i] = byte('0' + x)
+		} else {
+			errz.HardAssert(false, "invalid snafu array (%v) idx(%v) byte(%v)", b5, i, b5[i])
+		}
+	}
+	return string(snafu)
 }
