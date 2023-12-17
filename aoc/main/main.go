2cd3dd28f2738b3d2680047eac3564a851b9b7fe
avertocle <avertocle@gmail.com>
2023-12-18 03:15:10 +0530

    2023 day 16

--- aoc/main/main.go
+++ aoc/main/main.go
@@ -6,7 +6,7 @@ package main
 
 import (
 	"fmt"
-	prob "github.com/avertocle/contests/aoc/2023/day12"
+	prob "github.com/avertocle/contests/aoc/2023/day16"
 	"github.com/avertocle/contests/io/clr"
 	"github.com/avertocle/contests/io/errz"
 	"github.com/avertocle/contests/io/iutils"
@@ -24,7 +24,7 @@ func main() {
 	inputFileNames, err := iutils.GetInputFileList(dirPath)
 	errz.HardAssert(err == nil, "error fetching input file : dir(%v) | %v", dirPath, err)
 
-	inputFileNames = []string{"input_small.txt"}
+	//inputFileNames = []string{"input_small.txt"}
 	//inputFileNames = []string{"input_final.txt"}
 
 	var ansP1, ansP2 string
