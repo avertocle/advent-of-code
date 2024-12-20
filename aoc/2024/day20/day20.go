339f7e449daa063a768593ac9c275322fb47f8d5
avertocle <avertocle@gmail.com>
2024-12-20 23:39:46 +0530

    aoc 2024 day20 wip

--- aoc/2024/day20/day20.go
+++ aoc/2024/day20/day20.go
@@ -13,30 +13,90 @@ const DirPath = "../2024/day20"
 
 var gInput [][]byte
 
+type idx = arrz.Idx2D[int]
+
 const (
 	space = byte('.')
 	wall  = byte('#')
 )
 
 func SolveP1() string {
-	grid, start, end, costGrid, unvisited := initStuff()
-	findShortestPath(grid, start, end, costGrid, unvisited)
-	ans := costGrid[end.I][end.J]
+	ans := 0
+	basePathCost := findBaseCost()
+	cheatPointMap := xxx()
+	fmt.Println(len(cheatPointMap))
+	costSavingMap := make(map[int]int)
+	for _, v := range cheatPointMap {
+		grid, start, end, costGrid, unvisited := initStuff()
+		fmt.Printf("%v, %v, %v, %v : ", v[0].Str(), v[1].Str(), string(grid[v[0].I][v[0].J]), string(grid[v[1].I][v[1].J]))
+		grid[1][9] = space
+		grid[1][10] = space
+		findShortestPath(grid, start, end, costGrid, unvisited)
+		pathCost := costGrid[end.I][end.J]
+		savings := basePathCost - pathCost
+		fmt.Print(savings, v[0].Str(), v[1].Str(), string(grid[v[0].I][v[0].J]), string(grid[v[1].I][v[1].J]), pathCost)
+		fmt.Println()
+		costSavingMap[savings] += 1
+		break
+	}
+	for k, v := range costSavingMap {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+	ans = basePathCost
 	return fmt.Sprintf("%v", ans)
 }
 
-func xxx() {
+func findBaseCost() int {
+	grid, start, end, costGrid, unvisited := initStuff()
+	findShortestPath(grid, start, end, costGrid, unvisited)
+	return costGrid[end.I][end.J]
+}
+
+func xxx() map[string][]*idx {
+	m := make(map[string][]*idx)
 	for i := 0; i < len(gInput); i++ {
 		for j := 0; j < len(gInput[0]); j++ {
+			if gInput[i][j] == wall {
+				if i > 0 && gInput[i-1][j] == wall {
+					a := arrz.NewIdx2D(i, j)
+					b := arrz.NewIdx2D(i-1, j)
+					key := fmt.Sprintf("%v_%v", b.Str(), a.Str())
+					if _, ok := m[key]; !ok {
+						m[key] = []*idx{a, b}
+					}
+				} else if i < len(gInput)-1 && gInput[i+1][j] == wall {
+					a := arrz.NewIdx2D(i, j)
+					b := arrz.NewIdx2D(i+1, j)
+					key := fmt.Sprintf("%v_%v", a.Str(), b.Str())
+					if _, ok := m[key]; !ok {
+						m[key] = []*idx{a, b}
+					}
+				} else if j > 0 && gInput[i][j-1] == wall {
+					a := arrz.NewIdx2D(i, j)
+					b := arrz.NewIdx2D(i, j-1)
+					key := fmt.Sprintf("%v_%v", b.Str(), a.Str())
+					if _, ok := m[key]; !ok {
+						m[key] = []*idx{a, b}
+					}
+				} else if j < len(gInput[0])-1 && gInput[i][j+1] == wall {
+					a := arrz.NewIdx2D(i, j)
+					b := arrz.NewIdx2D(i, j+1)
+					key := fmt.Sprintf("%v_%v", a.Str(), b.Str())
+					if _, ok := m[key]; !ok {
+						m[key] = []*idx{a, b}
+					}
+				}
+			}
 		}
 	}
+	return m
 }
 
 func initStuff() ([][]byte, *arrz.Idx2D[int], *arrz.Idx2D[int], [][]int, cmz.MapVisited) {
 	grid := arrz.Copy2D(gInput)
 	s := arrz.NewIdx2D(arrz.Find2D(grid, 'S')[0]...)
 	e := arrz.NewIdx2D(arrz.Find2D(grid, 'E')[0]...)
-	costGrid := arrz.Init2D(len(grid), len(grid[0]), math.MaxInt)
+	costGrid := arrz.Init2D(len(grid), len(grid[0]), math.MaxInt/2)
 	costGrid[s.I][s.J] = 0
 	unvisited := initUnvisited(grid)
 	return grid, s, e, costGrid, unvisited
